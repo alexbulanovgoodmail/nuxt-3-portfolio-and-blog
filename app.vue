2d4df431a6064b0c07a62920a3ef22aab4913ecb
alexbulanovgoodmail <alexbulanovgoodmail@gmail.com>
2024-01-19 10:24:07 +0300

    Layouts

--- app.vue
+++ app.vue
@@ -1,18 +1,7 @@
 <template>
 	<div>
-		<nav>
-			<ul>
-				<li>
-					<NuxtLink to="/">Home</NuxtLink>
-				</li>
-				<li>
-					<NuxtLink to="/about">About</NuxtLink>
-				</li>
-				<li>
-					<NuxtLink to="/blog">Blog</NuxtLink>
-				</li>
-			</ul>
-		</nav>
-		<NuxtPage />
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
 	</div>
 </template>
