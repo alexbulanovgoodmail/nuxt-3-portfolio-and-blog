428ecc66ef1a97b8ed0ff8c568d0141b30de350a
alexbulanovgoodmail <alexbulanovgoodmail@gmail.com>
2024-01-25 17:41:17 +0300

    Vue Blog Post List Redesign #1 The Look

--- components/content/BlogPosts.vue
+++ components/content/BlogPosts.vue
@@ -8,11 +8,24 @@ const { data: posts } = await useAsyncData('blog-list', () =>
 </script>
 
 <template>
-	<div class="not-prose">
+	<div class="not-prose font-mono">
+		<div class="column text-sm text-gray-400">
+			<div>date</div>
+			<div>title</div>
+		</div>
 		<ul>
 			<li v-for="post in posts" :key="post._path">
-				<NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+				<NuxtLink class="column hover:bg-gray-100 dark:hover:bg-gray-800" :to="post._path">
+					<div class="text-gray-500">2023</div>
+					<div>{{ post.title }}</div>
+				</NuxtLink>
 			</li>
 		</ul>
 	</div>
 </template>
+
+<style scoped lang="scss">
+.column {
+	@apply border-b border-gray-200 flex items-center space-x-8 py-2 dark:border-gray-700;
+}
+</style>
