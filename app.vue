4789d9c8f1ca81d6f33e24ae730371be1e70b527
alexbulanovgoodmail <alexbulanovgoodmail@gmail.com>
2024-01-31 16:31:42 +0300

    Page Transitions

--- app.vue
+++ app.vue
@@ -7,3 +7,23 @@
 		</NuxtLayout>
 	</div>
 </template>
+
+<style>
+.page-enter-active {
+	transition:
+		filter 0.25s ease,
+		opacity 0.25s ease;
+}
+
+.page-leave-active {
+	transition:
+		filter 0.25s ease,
+		opacity 0.25s ease 0.15s;
+}
+
+.page-enter-from,
+.page-leave-to {
+	opacity: 0;
+	filter: grayscale(1);
+}
+</style>
